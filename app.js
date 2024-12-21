d0a7a372c662bf2999dad89f025b34ca72efcbc1
Matanda Phiri <matandaphiri64@gmail.com>
2024-12-21 02:45:55 -0600

    Add functions to app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const myLibrary = []
+
+function Book(title, author, pages, read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function(){
+        `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+    }
+}
+
+function addBookToLibrary(title, author, pages, read){
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, "read");
+addBookToLibrary("1984", "George Orwell", 328, "not read");
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, "read");
+addBookToLibrary("Moby Dick", "Herman Melville", 635, "not read");
+addBookToLibrary("Pride and Prejudice", "Jane Austen", 279, "read");
+
