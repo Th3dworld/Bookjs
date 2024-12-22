e42d4e108653d056f6ebbf9aa08511ffbf962aa0
Matanda Phiri <matandaphiri64@gmail.com>
2024-12-21 19:15:13 -0600

    Update project

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 const tableBody = document.getElementById("books");
 const addBookBtn = document.getElementById("add-book");
 
+//create variables
 const myLibrary = []
 
+//Logic functions
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -19,6 +21,13 @@ function addBookToLibrary(title, author, pages, read){
     myLibrary.push(newBook);
 }
 
+function showBooks(){
+    myLibrary.forEach(book => tableBody.innerHTML += `
+        <tr><td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.read}</td></tr>
+    `);
+}
+
+
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, "read");
 addBookToLibrary("1984", "George Orwell", 328, "not read");
 addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, "read");
@@ -26,10 +35,8 @@ addBookToLibrary("Moby Dick", "Herman Melville", 635, "not read");
 addBookToLibrary("Pride and Prejudice", "Jane Austen", 279, "read");
 
 
-function showBooks(){
-    myLibrary.forEach(book => tableBody.innerHTML += `
-        <tr><td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.read}</td></tr>
-    `);
-}
-
 showBooks()
+
+addEventListener(onclick, ()=>{
+    
+});
